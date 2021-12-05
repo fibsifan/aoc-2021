2f58eaaab257fb438c058268072b9dca5bea674f
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2021-12-05 14:16:03 +0100

    Introduce mechanism for solution testing.

--- src/Day.kt
+++ src/Day.kt
@@ -1,12 +1,17 @@
 import java.io.File
 
-abstract class Day {
-    val input: List<String> = File("src", "${this::class.simpleName}.txt").readLines()
+abstract class Day(val test: Boolean = false, val expected1: Int = 0, val expected2: Int = 0) {
+    val input: List<String> = File("src", "${this::class.simpleName}${if (test) "_test" else ""}.txt")
+        .readLines()
 
     abstract fun part1(): Int
     abstract fun part2(): Int
     fun run() {
         println(part1())
         println(part2())
+        if (test) {
+            check(expected1 == part1())
+            check(expected2 == part2())
+        }
     }
 }
