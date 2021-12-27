fe8542ad42a968c7ecbebd15c76d4b2a0213f6d8
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2021-12-27 22:13:17 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.6.10"
 }
 
 repositories {
@@ -11,7 +11,7 @@ dependencies {
 
 tasks {
     wrapper {
-        gradleVersion = "7.3"
+        gradleVersion = "7.3.3"
     }
 }
 
